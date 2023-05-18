5ffb06c44e36630b25f52439ec219437a8ac3e4d
Luan Otávio <locsilva@sga.pucminas.br>
2023-05-18 00:45:39 -0300

    feat: test-checkbox

--- src/components/Checkbox/Checkbox.spec.tsx
+++ src/components/Checkbox/Checkbox.spec.tsx
@@ -1,5 +1,5 @@
 import { Checkbox } from './Checkbox';
-import { render, screen } from '@/test/utils';
+import { render, screen, userEvent } from '@/test/utils';
 
 describe('Component: Checkbox', () => {
   it('should be able to render', () => {
@@ -8,5 +8,15 @@ describe('Component: Checkbox', () => {
     const componentUnderTest = screen.getByLabelText(/teste/i);
 
     expect(componentUnderTest).toBeInTheDocument();
+  });
+
+  it('should check on click on label', async () => {
+    render(<Checkbox label='teste' />);
+
+    const componentUnderTest = screen.getByLabelText(/teste/i);
+
+    await userEvent.click(componentUnderTest);
+
+    expect(componentUnderTest).toBeChecked();
   });
 });
