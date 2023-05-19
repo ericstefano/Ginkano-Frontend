132d6b41505aafe02ce88614a8c96fcee1ba8a3e
ericstefano <ericstefano12@gmail.com>
2023-05-19 14:13:40 -0300

    feat: add vitest ui and move vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { resolve } from 'path';
 import UnoCSS from '@unocss/vite';
 import react from '@vitejs/plugin-react';
@@ -5,6 +6,18 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), UnoCSS()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    coverage: {
+      provider: 'c8',
+      lines: 60,
+      functions: 60,
+      branches: 60,
+      statements: 60,
+    },
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
