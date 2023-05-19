b3634248cd77a68226bcbc2d85427c6db5b03c62
Luan Otávio <locsilva@sga.pucminas.br>
2023-05-19 12:37:11 -0300

    chore: change header should render test

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,9 +1,3 @@
 describe('header', () => {
-  it('should render', () => {
-    //    render(<Header> Teste</Header>);
-    //    const header = screen.getByText(/Teste/i);
-    //    expect(header).toBeInTheDocument();
-
-    expect(1).toBe(1);
-  });
+  it.todo('should render');
 });
