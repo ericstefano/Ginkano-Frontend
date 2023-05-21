d9e4ef9f8af8a529bbfe8e7e3c1268e5c41c3ea9
ericstefano <ericstefano12@gmail.com>
2023-05-21 19:52:44 -0300

    feat: improve Card components

--- src/components/SchoolCard/SchoolCard.tsx
+++ src/components/SchoolCard/SchoolCard.tsx
@@ -1,27 +1,60 @@
 import { ComponentPropsWithoutRef, ReactNode } from 'react';
+import { Link } from 'react-router-dom';
 import { BaseCard } from '../BaseCard';
+import { Edit } from '../icons/Edit';
+import { Trash } from '../icons/Trash';
 
 export type SchoolCardProps = {
   title: string;
   subtitle?: ReactNode;
+  to?: string;
   src: string;
   alt: string;
+  onUpdateButtonClick: () => void;
+  onDeleteButtonClick: () => void;
 } & ComponentPropsWithoutRef<'div'>;
 
 export const SchoolCard = ({
   src,
   alt,
+  to,
   title,
+  className,
+  onUpdateButtonClick,
+  onDeleteButtonClick,
   subtitle,
   ...props
 }: SchoolCardProps) => {
+  const LinkOrDiv = to ? Link : 'div';
+
   return (
-    <BaseCard.Root className='hover:(shadow-sm scale-100.5)' {...props}>
-      <BaseCard.Image src={src} alt={alt} />
-      <BaseCard.Description>
-        <BaseCard.Title>{title}</BaseCard.Title>
-        {subtitle ? <BaseCard.Subtitle>{subtitle}</BaseCard.Subtitle> : null}
-      </BaseCard.Description>
+    <BaseCard.Root className={className}>
+      <BaseCard.FloatButton
+        className='top-2 right-2 bg-violet-400'
+        onClick={onUpdateButtonClick}
+      >
+        <Edit className='h-5 w-5' />
+      </BaseCard.FloatButton>
+      <BaseCard.FloatButton
+        className='top-2 right-13 bg-red-400'
+        onClick={onDeleteButtonClick}
+      >
+        <Trash className='h-5 w-5' />
+      </BaseCard.FloatButton>
+      <LinkOrDiv to={to as string}>
+        <BaseCard.Container
+          className='hover:(shadow-sm scale-100.5)'
+          {...props}
+        >
+          <BaseCard.Image src={src} alt={alt} />
+          <BaseCard.Description>
+            <BaseCard.Title>{title}</BaseCard.Title>
+            {subtitle ? (
+              <BaseCard.Subtitle>{subtitle}</BaseCard.Subtitle>
+            ) : null}
+          </BaseCard.Description>
+        </BaseCard.Container>
+      </LinkOrDiv>
     </BaseCard.Root>
   );
 };
